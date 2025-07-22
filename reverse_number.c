50d9b960c57151586a1a7eb8d2a84408b2aaf033
CurryKARKI12 <azerati.1234@gmail.com>
2025-07-22 21:52:33 +0100

    Update reverse_number.c

--- reverse_number.c
+++ reverse_number.c
@@ -11,7 +11,7 @@ int main() {
 
     while (n != 0) {  // tant que n n'est pas égal a zero alors on execute les instructions de la boucle comme suit 
         reversed = reversed * 10 + n % 10; // n% 10 recupére le dernider chiffre de n,reversed *10 +...ajoute ce chiffre a la fin de reversed et n/10 supprime le dernier chiffre de n
-        n = n / 10;
+        n = n / 10; // reversed = 432 * 10 + 1 = 4320 + 1 = 4321 ; NB = ici 1 represente le dernier chiffre apres inversion et 1 represente le dernier chiffre avant inversion  
     }
 
     printf("Nombre inversé : %d\n", reversed);
