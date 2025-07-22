b9a06a3f383dc938d4ddbc4ede8fb65a57842af0
CurryKARKI12 <azerati.1234@gmail.com>
2025-07-22 20:12:28 +0100

    Update count_digits.c

--- count_digits.c
+++ count_digits.c
@@ -9,14 +9,14 @@ int main() {
     // Lecture de l'entier
     scanf("%d", &n);
 
-    int original = n;
+    int original = n; // ici on garde une copie de la valeur entrée par UI pour l'affichage a la fin 
 
     if (n == 0) {
-        count = 1;
+        count = 1; // si n = 0 alors le compteur considerera la réponse 1 sinon la boucle suivante ne fonctionne pas 
     } else {
-        while (n != 0) {
+        while (n != 0) {  // cette boucle divide le nombre par 10 a chaque itération ce qui décale un chiffre a droite (345/10 =34/10=3;3/10=0)   
             n = n / 10;
-            count++;
+            count++; // a la fin count donnera le nombre total de chiffre grace a count 
         }
     }
 
