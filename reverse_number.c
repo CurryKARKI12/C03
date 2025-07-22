1673123371087bac05d08b6c8d69334d942024f0
CurryKARKI12 <azerati.1234@gmail.com>
2025-07-22 20:36:13 +0100

    Update reverse_number.c

--- reverse_number.c
+++ reverse_number.c
@@ -4,13 +4,13 @@
 #include <stdio.h>
 
 int main() {
-    int n, reversed = 0;
+    int n, reversed = 0; //le nombre inversé initialisé a 0
 
     // Lecture du nombre
     scanf("%d", &n);
 
-    while (n != 0) {
-        reversed = reversed * 10 + n % 10;
+    while (n != 0) {  // tant que n n'est pas égal a zero alors on execute les instructions de la boucle comme suit 
+        reversed = reversed * 10 + n % 10; // n% 10 recupére le dernider chiffre de n,reversed *10 +...ajoute ce chiffre a la fin de reversed et n/10 supprime le dernier chiffre de n
         n = n / 10;
     }
 
